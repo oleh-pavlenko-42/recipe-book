bb9adf4cc1fc05dcfa33e551ac29757aefc3e420
Oleh Pavlenko <oleh.pavlenko.42@gmail.com>
2024-10-05 11:48:34 +0300

    added Header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  standalone: true,
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+})
+export class HeaderComponent {}
